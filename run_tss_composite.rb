ae9b02a35d39b7fc11e020a843a44d8607dfba13
Ron Gejman <rsg2119@columbia.edu>
2010-10-21 17:59:44 -0400

    updated run_tss_composite

--- run_tss_composite.rb
+++ run_tss_composite.rb
@@ -3,7 +3,7 @@ require 'constants'
 require 'mysql'
 
 def count_scores(tss_coords, file, output_file)
-  scores = Array.new(2000, 0.0)
+  scores = Array.new(2001, 0.0) #including the "0" position there are 2000 positions.
   Open3.popen3("gunzip -c #{file}") { |stdin, stdout, stderr|
     while (line = stdout.gets) #for some reason gunzip output to stderr via Open3.popen3
       if line[0,1] == "t" #the 1st header line starts with "track"
@@ -16,12 +16,9 @@ def count_scores(tss_coords, file, output_file)
       for l,r,s in tss_coords[chr]
         if l <= pos and r >= pos
           if s == "+" #strand
-            scores[2000 - r - pos] += t[1].to_f  # get the pos relative to the coord_pairs (0-based) and add the score to this pos.
+            scores[2000 - (r - pos)] += t[1].to_f  # get the pos relative to the coord_pairs (0-based) and add the score to this pos.
           else
-            if scores[r - pos - 1] == nil
-              throw "#{l} #{r} #{pos} #{r-pos}"
-            end
-            scores[r - pos-1] += t[1].to_f
+            scores[r - pos] += t[1].to_f
           end
         end
       end
@@ -38,26 +35,28 @@ end
 conn = Mysql::new(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB)
 res = conn.query("SELECT * FROM analysis_pairs WHERE active=1 ORDER BY created_at asc")
 res.each_hash do |row|
-  f_name = row["foreground"]
-  b_name = row["background"]
-  analysis_folder_name = f_name + "_" + b_name
-  analysis_folder_path = "#{QUEST_FOLDER}/#{analysis_folder_name}"
-  next unless File.exists? analysis_folder_path #has not yet been analyzed.
-  composite_plot_path = "#{COMPOSITE_PLOTS_FOLDER}/#{analysis_folder_name}_tss.png"
+  f_name                      = row["foreground"]
+  b_name                      = row["background"]
+  analysis_folder_name        = f_name + "_" + b_name
+  quest_analysis_folder_path  = "#{QUEST_FOLDER}/#{analysis_folder_name}"
+  composite_plot_path         = "#{COMPOSITE_PLOTS_FOLDER}/#{analysis_folder_name}/tss.png"
+  running_file                = running_file(analysis_folder_name, "make_tss_composite")
+  tmp_folder                  = "#{TMP_FOLDER}/#{analysis_folder_name}"
+  final_folder_path           = "#{COMPOSITE_PLOTS_FOLDER}/#{analysis_folder_name}"
+  f_wig_path                  = "#{quest_analysis_folder_path}/tracks/wig_profiles/ChIP_normalized.profile.wig.gz"
+  b_wig_path                  = "#{quest_analysis_folder_path}/tracks/wig_profiles/background_normalized.profile.wig.gz"
+  
   next if File.exists? composite_plot_path # the composite plot has already been generated, so this is done.
-  running_file        = running_file(analysis_folder_name, "make_tss_composite")
-  tmp_folder          = "#{TMP_FOLDER}/#{analysis_folder_name}"
   next if File.exists? running_file #This is being processed
-  # Use normalized profiles to compare the two!
-  f_wig_path = "#{analysis_folder_path}/tracks/wig_profiles/ChIP_normalized.profile.wig.gz"
-  b_wig_path = "#{analysis_folder_path}/tracks/wig_profiles/background_normalized.profile.wig.gz"
   next unless File.exists? f_wig_path
   next unless File.exists? b_wig_path
+  
+  Dir.chdir(TMP_FOLDER)
+  `mkdir -p #{tmp_folder}`
+  `touch #{running_file}`
+  TSS_COORDS = {}
+  
   begin
-    Dir.chdir(TMP_FOLDER)
-    `mkdir -p #{tmp_folder}`
-    `touch #{running_file}`
-    TSS_COORDS = {}
     # Put the TSS coordinates into a data structure (array of start/end pairs in hashmap keyed on chromosome)
     File.readlines("#{USEFUL_BED_FILES}/mm9.tss.2kb.bed").each {|line|
       tokens = line.split("\t")
@@ -70,14 +69,17 @@ res.each_hash do |row|
     count_scores(TSS_COORDS, b_wig_path, "#{tmp_folder}/scores_b.txt") if child2.nil? # child2 is nil if the thread is the 2nd fork.
     Process.waitall
     exit if child1.nil? or child2.nil? #if you are either one of the children, exit here.
-    #`mv #{tmp_folder} #{analysis_folder_path}`
+    #control script continues here.
+    
+    #Dir.chdir(tmp_folder)
+    
+    
+    #{}`mv #{tmp_folder} #{COMPOSITE_PLOTS_FOLDER}/`
   ensure
     Process.kill child1 unless child1.nil?
     Process.kill child2 unless child2.nil?
-    #FileUtils.rm(tmp_folder,      :force=>true)
+    FileUtils.rm(tmp_folder,      :force=>true)
     FileUtils.rm(running_file,    :force=>true)
-    t1.kill()
-    t2.kill()
   end
   break # We break so that other scripts have a chance to execute before we try this one again.
 end
