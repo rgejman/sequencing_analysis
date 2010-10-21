5a39f61962477ae042e8888a3782b6a067f0b31f
Ron Gejman <rsg2119@columbia.edu>
2010-10-21 19:20:24 -0400

    updated run_tss_composite

--- run_tss_composite.rb
+++ run_tss_composite.rb
@@ -37,18 +37,7 @@ def count_scores(tss_coords_file, folder, output_file)
       end
        # this is much faster than using delete_if because we don't have to iterate through every entry in tss_coords
        # and we only delete if we know there is stuff to delete.
-      if need_to_delete
-        i = 0
-        len = tss_coords[chr].length
-        while i < len
-          break if pos < tss_coords[chr][i][1]
-          if tss_coords[chr][i][1] < pos
-            tss_coords[chr].delete_at(i)
-            i = i - 1
-          end
-          i = i + 1
-        end
-      end
+      tss_coords[chr].delete_if{|a| a[1]< pos} if need_to_delete
       if n % 100 == 0
         File.open(output_file, "w") do |f|
           for score in scores
