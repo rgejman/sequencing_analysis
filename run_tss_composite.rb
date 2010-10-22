25dd284d5555c7db05a3c4a66c6c10bc8b72511c
Ron Gejman <rsg2119@columbia.edu>
2010-10-22 13:00:40 -0400

    mispelled marshal

--- run_tss_composite.rb
+++ run_tss_composite.rb
@@ -47,7 +47,7 @@ def count_scores(tss_coords_file, folder, output_file)
       end
       if child_id.nil? # if we are the child
         rd.close
-        wr.write Marshal.dump(scores.pack("m"))
+        wr.write Marshal.dump(scores.pack("M"))
         wr.close
         exit(0)
       end
@@ -56,7 +56,7 @@ def count_scores(tss_coords_file, folder, output_file)
   #only parent should reach here
   for rd,wr in pipes
     wr.close
-    loc_scores = Marshal.load(rd.read.unpack("m"))
+    loc_scores = Marshal.load(rd.read.unpack("M"))
     rd.close
     for i in 0...scores
       scores[i] += loc_scores[i]
