7bb42e48d525852221150100159b772d1d947eca
Ron Gejman <rsg2119@columbia.edu>
2010-10-22 11:05:17 -0400

    Reduced plot size of TSS figure

--- make_composite_tss_plot.r
+++ make_composite_tss_plot.r
@@ -1,11 +1,10 @@
-
 f = as.list(read.table("scores_f.txt"))$V1
 b = as.list(read.table("scores_b.txt"))$V1
 
 x = as.list(seq(-1000,1000))
 pdf("tss.pdf")
-plot(x=x,y=f, col="red")
-points(x=x,y=b)
+plot(x=x,y=f, col="red", pch=20, cex=0.5)
+points(x=x,y=b, pch=20, cex=0.5)
 
 legend_v = c("ChIP", "Control")
-legend("topright", legend_v, pch=1, col=c("red","black"))
+legend("topright", legend_v, pch=20, cex=0.5, col=c("red","black"))
