d2d7bc97fd0e4dbcf93d146ff5e4c413ae89260c
Ron Gejman <rsg2119@columbia.edu>
2010-10-22 14:14:02 -0400

    mispelled marshal

--- run_tss_composite.rb
+++ run_tss_composite.rb
@@ -48,10 +48,12 @@ def count_scores(tss_coords_file, folder, output_file)
         end
 
       end
-      wr.print scores.join(",")
-      wr.close
-      exit(0)
     end
+  end
+  if child_id.nil?
+    wr.print scores.join(",")
+    wr.close
+    exit(0)
   end
   #only parent should reach here
   `echo waiting for procs to finish >> /media/bigdisk/sequencing/tmp/out.log`
