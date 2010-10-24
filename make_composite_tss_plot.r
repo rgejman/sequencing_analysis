2daa80fa4d1e0bc012d569d34ff96c7e6cde3236
Ron Gejman <rsg2119@columbia.edu>
2010-10-23 20:25:42 -0400

    Set ylim on composite plot

--- make_composite_tss_plot.r
+++ make_composite_tss_plot.r
@@ -4,7 +4,8 @@ b = as.list(read.table("scores_b.txt"))$V1
 x = as.list(seq(-1000,1000))
 filename = paste(tail(strsplit(getwd(),"/")[[1]],1),"tss.pdf", sep=" ")
 pdf(filename)
-plot(x=x,y=f, col="red", pch=20, cex=0.5, xlab="Distance from TSS", ylab="Cumulative Score")
+ymax = max(f,b)
+plot(x=x,y=f, col="red", pch=20, cex=0.5, xlab="Distance from TSS", ylab="Cumulative Score", ylim=c(0,ymax))
 points(x=x,y=b, pch=20, cex=0.5)
 
 legend_v = c("ChIP", "Control")
