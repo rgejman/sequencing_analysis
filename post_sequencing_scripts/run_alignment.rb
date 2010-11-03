a0c76b27a7308e44dcd2b967a0a8247accd88a31
Ron Gejman <rsg2119@columbia.edu>
2010-11-03 19:24:32 -0400

    Updated run_alignment to take arguments

--- post_sequencing_scripts/run_alignment.rb
+++ post_sequencing_scripts/run_alignment.rb
@@ -19,7 +19,7 @@ Dir.foreach("#{FASTQ_FOLDER}/") do |file|
   puts file
   `touch #{running_file}`
   begin
-    `bowtie -p #{BT_NUM_THREADS} --best -m 1 #{GENOME} -o #{BT_OVERRIDE_OFFRATE} --sam "#{FASTQ_FOLDER}/#{file}" "#{tmp_file}"`
+    `bowtie -p #{BT_NUM_THREADS} --best -m 1 #{GENOME} --sam "#{FASTQ_FOLDER}/#{file}" "#{tmp_file}"`
     FileUtils.mv(tmp_file, output_file)
   rescue => e
     FileUtils.rm(output_file,     :force=>true)
