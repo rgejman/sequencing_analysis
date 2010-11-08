0eb7ddd45ce5fc0d2fe8abab89e38641fa78e61e
Ron Gejman <rsg2119@columbia.edu>
2010-11-08 15:01:45 -0500

    Added RNA-Seq tophat runner

--- post_sequencing_scripts/run_tophat.rb
+++ post_sequencing_scripts/run_tophat.rb
@@ -8,7 +8,7 @@ conn = Mysql::new(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB)
 res = conn.query("SELECT * FROM rna_seq_alignment ORDER BY created_at desc")
 res.each_hash do |rna_seq_alignment|
   output_folder_name  = rna_seq_alignment["person"] + "_" + rna_seq_alignment["sample"]
-  output_folder_path  = "#{TOPHAT_FOLDER}/#{analysis_folder_name}"
+  output_folder_path  = "#{TOPHAT_FOLDER}/#{output_folder_name}"
   next if File.exists? output_folder_path #this has already been analyzed. 
   files_res = conn.query("SELECT * FROM rna_seq_pairs WHERE rna_seq_alignment_id = #{rna_seq_alignment['id']}")
   pairs = []
@@ -34,4 +34,9 @@ end
 #-r = mean distance between ends of paired reads. e.g. read_length=36 & fragment_mean=300 => dist = 228
 #-p = num_threads
 
-`tophat -p #{NUM_THREADS} #{mean_dist_arg} #{files_arg} -G #{USEFUL_BED_FILES}/mm9.ucsc.genes.gtf --library-type fr-unstranded`
+GTF_FILE_ARG = "-G #{USEFUL_BED_FILES}/mm9.ucsc.genes.gtf"
+LIBRARY_TYPE_ARG = "--library-type fr-unstranded"
+OUTPUT_FOLDER_ARG = "-o #{output_folder_path}"
+cmd = "tophat -p #{NUM_THREADS} #{mean_dist_arg} #{GTF_FILE_ARG} #{LIBRARY_TYPE_ARG} #{OUTPUT_FOLDER_ARG} #{files_arg}"
+puts cmd
+`cmd`
