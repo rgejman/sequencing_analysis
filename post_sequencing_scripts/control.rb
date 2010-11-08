bfcc3c61cd9ca477d31ddc125a78d1a492cee68f
Ron Gejman <rsg2119@columbia.edu>
2010-11-08 17:57:14 -0500

    Updated control script

--- post_sequencing_scripts/control.rb
+++ post_sequencing_scripts/control.rb
@@ -19,14 +19,18 @@ programs = ["run_fastqc.rb", "run_alignment.rb", "run_sort_sam.rb",
                 "run_macs_single.rb","run_macs_pair.rb", "run_quest.rb",
                 "run_tophat.rb"] # #,"run_ceas.rb" => Need to make sure is integrated.
               #,"run_make_bed.rb","run_make_wig.rb" # unclear if these are necessary
+              
+def forks
+  Dir.entries("#{LOG_FOLDER}/").select{|e| e =~ /\.pid/ }.length
+end
 
 loop do
   for p in programs
-    forks = 0
-    while (forks = Dir.entries("#{LOG_FOLDER}/").select{|e| e =~ /\.pid/ }.length) > MAX_FORKS
-      puts "#{forks} running"
+    while forks >= MAX_FORKS
+      puts "#{forks} running. #{MAX_FORKS} max."
       sleep(30)
     end
+    puts "#{forks} running. #{MAX_FORKS} max."
     call(p, options)
     puts "Called #{p}"
   end
