85b13e400a9877a17de6c55700701b57b7f01c3f
Ron Gejman <rsg2119@columbia.edu>
2010-11-08 17:31:18 -0500

    Updated control script

--- post_sequencing_scripts/control.rb
+++ post_sequencing_scripts/control.rb
@@ -16,7 +16,9 @@ options = {
 
 forks = []
 programs = ["run_fastqc.rb", "run_alignment.rb", "run_sort_sam.rb",
-              "run_bam_index.rb", "run_igvtools.rb", "run_make_bam.rb", "run_macs_single.rb","run_macs_pair.rb", "run_quest.rb"] # #,"run_ceas.rb" => Need to make sure is integrated.
+              "run_bam_index.rb", "run_igvtools.rb", "run_make_bam.rb",
+                "run_macs_single.rb","run_macs_pair.rb", "run_quest.rb",
+                "run_tophat.rb"] # #,"run_ceas.rb" => Need to make sure is integrated.
               #,"run_make_bed.rb","run_make_wig.rb" # unclear if these are necessary
 
 loop do
