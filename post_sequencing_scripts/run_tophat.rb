6b2e1263d5fd1ede40b74d18a813e5a7a4c66081
Ron Gejman <rsg2119@columbia.edu>
2010-11-08 15:05:13 -0500

    Added RNA-Seq tophat runner

--- post_sequencing_scripts/run_tophat.rb
+++ post_sequencing_scripts/run_tophat.rb
@@ -11,24 +11,24 @@ res.each_hash do |rna_seq_alignment|
   output_folder_path  = "#{TOPHAT_FOLDER}/#{output_folder_name}"
   next if File.exists? output_folder_path #this has already been analyzed. 
   files_res = conn.query("SELECT * FROM rna_seq_pairs WHERE rna_seq_alignment_id = #{rna_seq_alignment['id']}")
-  pairs = []
+  reads = []
   # for each file that comprises this RNA-Seq run (there may be multiple)
   files_res.each_hash do |file|
     if file["paired"].to_i == 1
-      pairs << [file["name"] + ".1.txt", file["name"] + ".2.txt"]
+      reads << [file["name"] + ".1.txt", file["name"] + ".2.txt"]
     else
-      pairs << file["name"] + ".txt"
+      reads << file["name"] + ".txt"
     end
   end
   read_length           = rna_seq_alignment["read_length"]
   mean_fragment_length  = rna_seq_alignment["mean_fragment_length"]
   mean_dist_arg         = ""
-  if paired
-    files_arg = pairs.collect {|p| p[0] }.join(",") + " " + pairs.collect {|p| p[1] }.join(",")
+  if reads[0].length == 2 #the first "reads entry" has 2 files, so it's paired.
+    files_arg = reads.collect {|p| p[0] }.join(",") + " " + reads.collect {|p| p[1] }.join(",")
     mean_dist           = mean_fragment_length - (read_length*2)
     mean_dist_arg       = "-r #{mean_dist_arg}"
   else
-    files_arg = pairs.collect {|p| p[0] }.join(",")
+    files_arg = reads.collect {|p| p[0] }.join(",")
   end
 end
 #-r = mean distance between ends of paired reads. e.g. read_length=36 & fragment_mean=300 => dist = 228
