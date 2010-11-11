31350d92c470307f0424eb9fda486d2bf774d6c5
Ron Gejman <rsg2119@columbia.edu>
2010-11-11 09:52:32 -0500

    updated cufflinks

--- post_sequencing_scripts/run_cufflinks.rb
+++ post_sequencing_scripts/run_cufflinks.rb
@@ -33,7 +33,7 @@ res.each_hash do |rna_seq_alignment|
   #LIBRARY_TYPE_ARG = "--library-type fr-unstranded" # this is the default
   `touch #{running_file}`
   begin
-    cmd = "tophat -p #{NUM_THREADS} #{mean_dist_arg} -o #{cufflinks_output_folder_path} --reference-seq #{BOWTIE_INDEXES}/#{GENOME}.fa #{accepted_hits}"
+    cmd = "tophat -p #{NUM_THREADS} #{mean_dist_arg} -o #{cufflinks_output_folder_path} -r #{BOWTIE_INDEXES}/#{GENOME}.fa #{accepted_hits}"
     puts cmd
     `#{cmd}`
   rescue => e
