554cd71e41b71b2a7d251e74259e1673883a8c13
Ron Gejman <rsg2119@columbia.edu>
2010-11-22 18:30:50 -0500

    Updated GTF file

--- post_sequencing_scripts/run_igvtools.rb
+++ post_sequencing_scripts/run_igvtools.rb
@@ -6,21 +6,29 @@ EXTEND_LENGTH = 100
 Dir.foreach("#{ALIGNMENTS_FOLDER}/") do |file|
   next unless file =~ /\.sorted.bam$/
   running_file          = running_file(file, "igvtools")
-  tmp_file              = "#{TMP_FOLDER}/#{file}.cov.tdf"
-  output_file           = "#{IGVTOOLS_OUTPUT_FOLDER}/#{file}.cov.tdf"
+  tmp_file1             = "#{TMP_FOLDER}/#{file}.cov.tdf"
+  tmp_file2             = "#{TMP_FOLDER}/#{file}.wig"
+  output_file_1         = "#{IGVTOOLS_OUTPUT_FOLDER}/#{file}.cov.tdf"
+  output_file_2         = "#{IGVTOOLS_OUTPUT_FOLDER}/#{file}.wig"
+  
   input_path            = "#{ALIGNMENTS_FOLDER}/#{file}"
   next if File.exists? output_file #Already processed
   next if File.exists? running_file #Being processed
   puts file
   `touch #{running_file}`
   begin
-    `igvtools count -e #{EXTEND_LENGTH} "#{input_path}" "#{tmp_file}" "#{GENOMES_FOLDER}/#{GENOME}.genome"`
-    FileUtils.mv(tmp_file, output_file)
+    `igvtools count -e #{EXTEND_LENGTH} "#{input_path}" "#{tmp_file1},#{tmp_file2}" "#{GENOMES_FOLDER}/#{GENOME}.genome"`
+    FileUtils.mv(tmp_file1, output_file1)
+    FileUtils.mv(tmp_file2, output_file2)
+    
   rescue => e
-    FileUtils.rm(output_file,     :force=>true)
+    FileUtils.rm(output_file1,     :force=>true)
+    FileUtils.rm(output_file2,     :force=>true)
+    
     throw e
   ensure
-    FileUtils.rm(tmp_file,        :force=>true)
+    FileUtils.rm(tmp_file1,        :force=>true)
+    FileUtils.rm(tmp_file2,        :force=>true)
     FileUtils.rm(running_file,    :force=>true)
   end
   break # We break so that other scripts have a chance to execute before we try this one again.
