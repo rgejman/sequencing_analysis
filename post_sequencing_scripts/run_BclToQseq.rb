375995dd212d5bc0b598423ff89e99ab9387d618
Ron Gejman <rsg2119@columbia.edu>
2010-11-30 15:08:42 -0500

    Added run_BclToQseq.rb

--- post_sequencing_scripts/run_BclToQseq.rb
+++ post_sequencing_scripts/run_BclToQseq.rb
@@ -7,6 +7,7 @@ NUM_THREADS = 24
 conn = Mysql::new(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB)
 res = conn.query("SELECT * FROM sequencing_run WHERE run_at IS NOT NULL and illumina_run_id IS NOT NULL ORDER BY run_at desc")
 res.each_hash do |sequencing_run|
+  seq_run_id                    = sequencing_run["id"]
   run_id                        = sequencing_run["illumina_run_id"]
   running_file                  = running_file(run_id, "BclToQseq")
   next if File.exists? running_file
@@ -14,7 +15,7 @@ res.each_hash do |sequencing_run|
   base_calls_folder             = "#{intensities_folder}/BaseCalls"
   output_folder                 = base_calls_folder
   samples = {}
-  samples_res = conn.query("SELECT * FROM sequencing_samples WHERE sequencing_run_id = '#{run_id}' ORDER BY lane desc")
+  samples_res = conn.query("SELECT * FROM sequencing_samples WHERE sequencing_run_id = '#{seq_run_id}' ORDER BY lane desc")
   date    = sequencing_run["run_at"][0,10].gsub("-","_")
   nsamples_already_converted = 0
   samples_res.each_hash do |sample|
