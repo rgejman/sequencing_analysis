e02bd83f5b225555c28fa5f3d51c9f94379954d0
Ron Gejman <rsg2119@columbia.edu>
2010-11-30 14:33:36 -0500

    Added run_BclToQseq.rb

--- analysis_scripts/cluster.r
+++ analysis_scripts/cluster.r
@@ -1,93 +1,135 @@
+remove_less_one_and_log = function(d) {
+	# Changes values <1 to 1 and log-transforms the data
+	# To do this we should set all RPKM values <1 to some constant (I chose 1 because log(1) = 0)
+	less_than_one = (d < 1)
+	d[less_than_one] = 1
+	d = log(d)
+	return(d)
+}
+
+make_wss_plot = function(data,name,n=25) {
+	# This plot helps us find the optimal # of clusters
+	wss <- (nrow(data)-1)*sum(apply(data,2,var))
+	for (i in 2:n) {
+		wss[i] <- sum(kmeans(data, iter.max=50,nstart=5, centers=i)$withinss)
+		print(paste("Cluster for WSS", i,"done",sep=" "))
+	}
+	pdf(paste("wss",".",name,".pdf",sep=""))
+	plot(1:n, wss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
+	dev.off()
+}
+
+#data = data.frame of all values
+#cols_per_heatmap = data.frame like this:
+# WTK9	KRK9	WTK4	KRK4
+# b0	b0		b0		b0
+# b1	b1		b1		b1
+# etc	etc		etc		etc
+make_heatmaps = function(data, symbol, cols_per_heatmap, clusters, postfix) {
+	for(cluster in clusters) {
+
+		######## DETERMINE # OF BREAKS ########
+		# e.g.
+		# K9 columns have mean=~2 and sd=~2 (without log)
+		# K4 columns have mean=~2 and sd that ranges from 7-25
+
+		max_mean_plus_sd = max(mean(data)+sd(data))
+		breaks = c(seq(from=0,to=max_mean_plus_sd,by=0.5),10000)
+		colors = rev(gray(0:(length(breaks)-2)/(length(breaks)-2)))
+
+		######## CLUSTERING ########
+
+		fit <- kmeans(data, cluster, iter.max=50,nstart=10)
+
+		# get cluster means 
+		# append cluster assignment
+		d <- data.frame(data, symbol)
+		d <- data.frame(d, fit$cluster)
+
+		d = d[order(d$fit.cluster),]
+
+		######## MAKE FIGURES ########
+
+		png(paste("heatmap-",cluster,".",postfix,".png",sep=""), res=300, height=44,width=34,units="in")
+		#pdf(paste("heatmap-",cluster,".pdf",sep=""))
+		
+		nheatmaps = ncol(cols_per_heatmap)
+
+		par(mfrow=c(1,nheatmaps))
+		
+		for(n in 1:nheatmaps) {
+			data_plot = t(d[,as.vector(cols_per_heatmap[,n])]) # transpose the array
+			nr = nrow(data_plot)
+			nc = ncol(data_plot)
+			image(1:nr,1:nc,data.matrix(data_plot),breaks=breaks,col=colors,axes=FALSE,xlab=colnames(cols_per_heatmap)[n],ylab="")
+		}
+		dev.off()
+
+		######## OUTPUT SYMBOLS ########
+
+		## Output the symbols in each group.
+		write.table(d,file=paste("cluster-",cluster,".",postfix,".txt",sep=""),sep="\t",quote=FALSE)
+
+		print(paste("Finished cluster", cluster,sep=" "))
+	}
+}
+
+
+
 num_blocks = 80
-base_col_names = unlist(lapply(0:(num_blocks-1), function(x) paste("b",x,sep="")))
+#base_col_names = unlist(lapply(0:(num_blocks-1), function(x) paste("b",x,sep="")))
 non_numeric_col_names = c("symbol","chr","start","end","strand")
 
 f1 = "Eugene_WT_H3K4me3_CD4.profile.6000.80.txt"
 f2 = "Eugene_KR_H3K4me3_CD4.profile.6000.80.txt"
+f3 = "Eugene_WT_H3K9me2_CD4_all.profile.6000.80.txt"
+f4 = "Eugene_KR_H3K9me2_CD4_all.profile.6000.80.txt"
 
 t1 = read.table(f1, header=TRUE,sep="\t",quote="")
 t2 = read.table(f2, header=TRUE,sep="\t",quote="")
-
-data1 = t1[-match(non_numeric_col_names,names(t1))]
-data2 = t2[-match(non_numeric_col_names,names(t2))]
-
-row.names(data1) = t1$symbol
-row.names(data2) = t2$symbol
-
-data = merge(data1,data2, all.x=FALSE,all.y=TRUE,sort=FALSE, by="row.names")
-# Remove the "row.names" column which was added automatically on merge
-data = data[,-(1:1)]
-
-# We will now log-transform the data.
-# To do this we should set all RPKM values <1 to some constant (I chose 1 because log(1) = 0)
-less_than_one = (data < 1)
-data[less_than_one] = 1
-data = log(data)
+t3 = read.table(f3, header=TRUE,sep="\t",quote="")
+t4 = read.table(f4, header=TRUE,sep="\t",quote="")
 
 
-# This plot helps us find the optimal # of clusters
-wss <- (nrow(data)-1)*sum(apply(data,2,var))
-for (i in 2:25) {
-	wss[i] <- sum(kmeans(data, iter.max=50,nstart=5, centers=i)$withinss)
-	print(paste("Cluster for WSS", i,"done",sep=" "))
-}
-pdf("wss.pdf")
-plot(1:25, wss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
-dev.off()
+WTK4 = t1[-match(non_numeric_col_names,names(t1))]
+KRK4 = t2[-match(non_numeric_col_names,names(t2))]
+WTK9 = t3[-match(non_numeric_col_names,names(t3))]
+KRK9 = t4[-match(non_numeric_col_names,names(t4))]
 
-clusters = c(10,15) #16,6,10,20,
 
-for(cluster in clusters) {
-	
-	######## DETERMINE # OF BREAKS ########
-	
-	# K9 columns have mean=~2 and sd=~2 (without log)
-	# K4 columns have mean=~2 and sd that ranges from 7-25
-	
-	# FOR K9 Columns:
-	
-	max_mean_plus_sd = max(mean(data)+sd(data))
-	#breaks = c(seq(from=0,to=(max(data)-1),by=(max(data)-1)/255),10000)
-	breaks = c(seq(from=0,to=max_mean_plus_sd,by=0.5),10000)
-	colors = rev(gray(0:(length(breaks)-2)/(length(breaks)-2)))
-	
-	# For K4 Columns:
-	#breaks = c(seq(from=0,to=25,by=1),10000)
-	#colors = rev(gray(0:(length(breaks)-2)/(length(breaks)-2)))
-	
-	######## CLUSTERING ########
-	
-	fit <- kmeans(data, cluster, iter.max=50,nstart=5)
-	# get cluster means 
-	#aggregate(data,by=list(fit$cluster),FUN=mean)
-	# append cluster assignment
-	d <- data.frame(data, t1$symbol)
-	names(d)[names(d)=="t1.symbol"] = "symbol"
-	d <- data.frame(d, fit$cluster)
+row.names(WTK4) = t1$symbol
+row.names(KRK4) = t2$symbol
+row.names(WTK9) = t3$symbol
+row.names(KRK9) = t4$symbol
 
-	d = d[order(d$fit.cluster),]
-	
-	png(paste("heatmap-",cluster,".png",sep=""), res=300, height=44,width=34,units="in")
-	#pdf(paste("heatmap-",cluster,".pdf",sep=""))
-	
-	par(mfrow=c(1,2))
-		
-	data_plot = t(d[,colnames(data1)]) # transpose the array
-	image(1:nrow(data_plot),1:ncol(data_plot),data.matrix(data_plot),breaks=breaks,col=colors,axes=FALSE,xlab="WT H4K9me2",ylab="")
-	data_plot = t(d[,colnames(data2)])
-	image(1:nrow(data_plot),1:ncol(data_plot),data.matrix(data_plot),breaks=breaks,col=colors,axes=FALSE,xlab="KR H4K9me2",ylab="")
-	dev.off()
-	
-	#
+WTK4_KRK4 			= merge(WTK4,KRK4, all.x=FALSE,all.y=TRUE,sort=FALSE, by="row.names")
+WTK4_KRK4 			= WTK4_KRK4[,-(1:1)] # Remove the "row.names" column which was added automatically on merge
 
-	#png(paste("heatmap-",cluster,".png",sep=""), res=300, height=44,width=34,units="in")
-	#par(mfrow=c(1,2))
-	#heatmap(data.matrix(d[colnames(data1),]),labRow=NA,Rowv=NA,Colv=NA,col=colors,scale="none",zlim=color_limits)
-	#heatmap(data.matrix(d[colnames(data2),]),labRow=NA,Rowv=NA,Colv=NA,col=colors,scale="none",zlim=color_limits)
-	#dev.off()
-	
-	## Output the symbols in each group.
-	write.table(d,file=paste("cluster-",cluster,".txt",sep=""),sep="\t",quote=FALSE)
-	
-	print(paste("Finished cluster", cluster,sep=" "))
-}
+WTK9_KRK9 			= merge(WTK9,KRK9, all.x=FALSE,all.y=TRUE,sort=FALSE, by="row.names")
+WTK9_KRK9 			= WTK9_KRK9[,-(1:1)] # Remove the "row.names" column which was added automatically on merge
+
+# Again, we have to assume that the rows are ordered by symbol name and that the symbol names are the same, etc.
+WTK4_KRK4_WTK9_KRK9 = merge(WTK4_KRK4,WTK9_KRK9, all.x=FALSE,all.y=TRUE,sort=FALSE, by="row.names")
+WTK4_KRK4_WTK9_KRK9 = WTK4_KRK4_WTK9_KRK9[,-(1:1)] # Remove the "row.names" column which was added automatically on merge
+
+# Changes values <1 to 1 and log-transform the data
+WTK4_KRK4 			= remove_less_one_and_log(WTK4_KRK4)
+WTK9_KRK9 			= remove_less_one_and_log(WTK9_KRK9)
+WTK4_KRK4_WTK9_KRK9 = remove_less_one_and_log(WTK4_KRK4_WTK9_KRK9)
+
+clusters = c(8,10,15) #16,6,10,20,
+
+make_wss_plot(WTK4_KRK4, "WTK4_KRK4")
+make_wss_plot(WTK9_KRK9, "WTK9_KRK9")
+make_wss_plot(WTK4_KRK4_WTK9_KRK9, "WTK4_KRK4_WTK9_KRK9")
+
+cols_per_heatmap = data.frame(WTK4=colnames(WTK4), KRK4=colnames(KRK4))
+make_heatmaps(WTK4_KRK4, t1$symbol, cols_per_heatmap, clusters, "WTK4_KRK4")
+
+cols_per_heatmap = data.frame(WTK9=colnames(WTK9), KRK9=colnames(KRK9))
+make_heatmaps(WTK9_KRK9, t1$symbol, cols_per_heatmap, clusters, "WTK9_KRK9")
+
+cols_per_heatmap = data.frame(WTK4=colnames(WTK4), KRK4=colnames(KRK4), WTK9=colnames(WTK9), KRK9=colnames(KRK9))
+make_heatmaps(WTK4_KRK4_WTK9_KRK9, t1$symbol, cols_per_heatmap, clusters, "WTK4_KRK4_WTK9_KRK9")
+
+
---
+++ post_sequencing_scripts/run_BclToQseq.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby -KU
+$: << File.expand_path(File.dirname(__FILE__) + "/../")
+require 'constants'
+require 'mysql'
+NUM_THREADS = 24
+
+conn = Mysql::new(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB)
+res = conn.query("SELECT * FROM sequencing_run WHERE run_at IS NOT NULL and illumina_run_id IS NOT NULL ORDER BY run_at desc")
+res.each_hash do |sequencing_run|
+  run_id                        = sequencing_run["illumina_run_id"]
+  running_file                  = running_file(run_id, "BclToQseq")
+  next if File.exists? running_file
+
+  intensities_folder            = "#{RAW_FOLDER}/#{run_id}/Data/Intensities"
+  base_calls_folder             = "#{intensities_folder}/BaseCalls"
+  output_folder                 = base_calls_folder
+
+  `touch running_file`
+  begin
+    cmd = "setupBclToQseq.py -i #{base_calls_folder} -p #{intensities_folder} -o #{output_folder} --in-place --overwrite"
+    `#{cmd}`
+    Dir.chdir(output_folder)
+    `make -j #{NUM_THREADS}`
+  rescue => e
+    throw e
+  ensure
+    FileUtils.rm(running_file,    :force=>true)
+    conn.close
+  end
+  break
+end
