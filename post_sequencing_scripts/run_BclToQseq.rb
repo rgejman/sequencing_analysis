f1e2198d6b9c1f8163c2272b2fc6c90f5ac9fb1a
Ron Gejman <rsg2119@columbia.edu>
2010-11-30 14:36:33 -0500

    Added run_BclToQseq.rb

--- post_sequencing_scripts/run_BclToQseq.rb
+++ post_sequencing_scripts/run_BclToQseq.rb
@@ -10,7 +10,6 @@ res.each_hash do |sequencing_run|
   run_id                        = sequencing_run["illumina_run_id"]
   running_file                  = running_file(run_id, "BclToQseq")
   next if File.exists? running_file
-
   intensities_folder            = "#{RAW_FOLDER}/#{run_id}/Data/Intensities"
   base_calls_folder             = "#{intensities_folder}/BaseCalls"
   output_folder                 = base_calls_folder
@@ -18,9 +17,12 @@ res.each_hash do |sequencing_run|
   `touch running_file`
   begin
     cmd = "setupBclToQseq.py -i #{base_calls_folder} -p #{intensities_folder} -o #{output_folder} --in-place --overwrite"
-    `#{cmd}`
+    puts cmd
+    #`#{cmd}`
     Dir.chdir(output_folder)
-    `make -j #{NUM_THREADS}`
+    cmd = "make -j #{NUM_THREADS}"
+    puts cmd
+    #`cmd`
   rescue => e
     throw e
   ensure
