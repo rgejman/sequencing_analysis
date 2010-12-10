175464f6543c2461e0ca70dc4b5ef8e3977047c7
Ron Gejman <rsg2119@columbia.edu>
2010-12-10 15:13:27 -0600

    Updated fastqc script to mv output to fastqc folder.

--- post_sequencing_scripts/run_fastqc.rb
+++ post_sequencing_scripts/run_fastqc.rb
@@ -4,27 +4,31 @@ require 'constants'
 files = Dir.entries("#{FASTQ_CHIP_FOLDER}/").collect{|e| "#{FASTQ_CHIP_FOLDER}/#{e}"} + Dir.entries("#{FASTQ_RNA_SEQ_FOLDER}/").collect{|e| "#{FASTQ_RNA_SEQ_FOLDER}/#{e}"}
 for path in files
   next unless path =~ /\.txt$/
-  name = File.basename(path).gsub(".txt","")
-  running_file = running_file(name, "fastqc")
-  fastqc_tmp_folder_path        = "#{path.gsub(".txt","")}_fastqc"
-  fastqc_output_folder_path     = "#{FASTQC_FOLDER}/#{name}"
+  name                          = File.basename(path).gsub(".txt","")
+  user                          = name.split("_")[0]
+  running_file                  = running_file(name, "fastqc")
+  fastqc_tmp_folder_path        = path.gsub(".txt","") + "_fastqc"
+  fastqc_tmp_zip_path           = "#{path}.zip"
+  fastqc_output_folder_path     = "#{FASTQC_FOLDER}/#{user}/#{name}_fastqc"
 
   next if File.exists? fastqc_output_folder_path # The file has been processed in the past
   next if File.exists? running_file #This is being processed
-
   puts name
   `touch #{running_file}`
   begin
-    `fastqc #{path}`
+    cmd = "fastqc #{path}"
+    `#{cmd}`
     cmd = "mv #{fastqc_tmp_folder_path} #{fastqc_output_folder_path}"
     puts cmd
     `#{cmd}`
   rescue => e
+    FileUtils.rm(fastqc_tmp_folder_path,        :force=>true)
+    FileUtils.rm(fastqc_tmp_zip_path,           :force=>true)
     FileUtils.rm(fastqc_output_folder_path,     :force=>true)
     throw e
   ensure
-    FileUtils.rm("#{fastqc_tmp_folder_path}.zip", :force=>true)
-    FileUtils.rm("#{fastqc_tmp_folder_path}",     :force=>true)
+    FileUtils.rm(fastqc_tmp_folder_path,        :force=>true)
+    FileUtils.rm(fastqc_tmp_zip_path,           :force=>true)
     FileUtils.rm(running_file,                    :force=>true)
   end
   break # We break so that other scripts have a chance to execute before we try this one again.
