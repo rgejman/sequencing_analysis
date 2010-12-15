eaed3b3a192f2b11a7206f44210d9fafc07928fb
Ron Gejman <rsg2119@columbia.edu>
2010-12-15 13:34:17 -0500

    Script for calculating the gene pausing index

--- analysis_scripts/gene_pausing_index.rb
+++ analysis_scripts/gene_pausing_index.rb
@@ -1,65 +1,44 @@
 #!/usr/bin/env ruby -wKU
 ## For each gene: calculates "Pausing Index" as defined by Muse et al (2007 Nat Genetics 29, 1507-1511)
-## Pausing_index = (PolII at TSS +/- 250bp) / (PolII +500bp to end of gene)
+## Pausing_index = log2((PolII at TSS +/- 250bp) - (PolII +500bp to end of gene))
 require 'wigreader'
 require 'pp'
 
-def remove_smaller_genes(arr)
-  arr.uniq! # To remove genes that have the same TSS and TES
-  f_arr = arr.clone
-  for a in (0...arr.length)
-    for b in ((a+1)...arr.length)
-      next unless arr[a][:gene] == arr[b][:gene]
-      a_len = (arr[a][:start].to_i - arr[a][:end].to_i).abs
-      b_len = (arr[b][:start].to_i - arr[b][:end].to_i).abs
-      if a_len < b_len
-        f_arr.delete(arr[a])
-      elsif
-        f_arr.delete(arr[b])
-      end
-    end
-  end
-  return f_arr
+class << Math
+  def log2(n); log(n) / log(2); end
 end
 
 WIG_FILE            = ARGV[0] ## This should be a RNA Pol II chip-seq wiggle file
-AROUND_TSS_AREA     = 250 # +/- this number is the area defined to be the paused-pol II area.
+US_TSS_OFFSET       = 250 # +/- this number is the area defined to be the paused-pol II area.
 TSS_DS_OFFSET       = 500 # TSS + this number until the end of the gene is the "DS" area
 
-GENE_BED_FILE       = "/home/tarakhovsky/genomics/useful_bed_files/mm9.refseq.genes.gtf"
-output_file         = File.basename(WIG_FILE).split(".")[0] + ".pausing_index.txt" #e.g. "Eugene_WT_H3K4me3_CD4.sorted.bam.wig" to "Eugene_WT_H3K4me3_CD4.profile.6000.80.txt"
+GENE_BED_FILE       = "/home/tarakhovsky/genomics/useful_bed_files/mm9.refseq.genes.uniq_longer.bed"
 
 block_len = block_len.to_i
-genes = File.readlines(GENE_BED_FILE).collect{|l| g={}; g[:chr],g[:start],g[:end],g[:gene],tmp,g[:strand]= l.chomp.split("\t");g}
+genes = File.readlines(GENE_BED_FILE).collect{|l| g={}; g[:chr],g[:start],g[:end],g[:symbol],tmp,g[:strand]= l.chomp.split("\t");g}
 genes = genes.reject {|g| g[:chr] =~ /random/ }
-
-## Some genes have multiple entries in this file because of differing splice patterns.
-## Choose the longer genes (as defined by TSS and Transcription termination site)
-## Why? Because you're less likely to have false positive paused polymerase genes 
-genes = remove_smaller_genes(genes)
-
 reader = WigReader.new(WIG_FILE)
 
 for gene in genes
   gene[:end]    = gene[:end].to_i
   gene[:start]  = gene[:start].to_i
-  pausing_start = gene[:start] - AROUND_TSS_AREA
-  pausing_end   = gene[:start] + AROUND_TSS_AREA
-  ds_start      = gene[:start] + TSS_DS_OFFSET
+  pausing_start = gene[:start] - US_TSS_OFFSET
+  pausing_end   = gene[:start] + TSS_DS_OFFSET
+  ds_start      = gene[:start] + TSS_DS_OFFSET + 1
   ds_end        = gene[:end]
   if gene[:strand] == "-"
-    pausing_start = gene[:start] + AROUND_TSS_AREA
-    pausing_end   = gene[:start] - AROUND_TSS_AREA
-    ds_start      = gene[:start] - TSS_DS_OFFSET
+    pausing_start = gene[:start] + US_TSS_OFFSET
+    pausing_end   = gene[:start] - TSS_DS_OFFSET
+    ds_start      = gene[:start] - TSS_DS_OFFSET - 1
     ds_end        = gene[:end]
   end
   begin
     gene[:pausing_area_rpkm]  = reader.fpkm(gene[:chr], pausing_start, pausing_end)
     gene[:ds_area_rpkm]       = reader.fpkm(gene[:chr], ds_start, ds_end)
-    gene[:pausing_index]      = gene[:pausing_area_rpkm] / gene[:ds_area_rpkm]
+    gene[:pausing_index]      = Math.log2(gene[:pausing_area_rpkm] - gene[:ds_area_rpkm])
   rescue => e
     puts e
-    puts "#{gene[:gene]} could not be found in the wig file."
+    puts "#{gene[:symbol]} could not be found in the wig file."
   end
 end
 
@@ -67,6 +46,6 @@ File.open(output_file, "w") do |f|
   f.print "symbol\tchr\tstart\tend\tstrand\tpausing_index\tpromotor_rpkm\tds_rpkm"
   f.print "\n"
   for gene in genes
-    f.puts "#{gene[:gene]}\t#{gene[:chr]}\t#{gene[:start]}\t#{gene[:end]}\t#{gene[:strand]}\t#{gene[:pausing_index]}\t#{gene[:pausing_area_rpkm]}\t#{gene[:ds_area_rpkm]}"
+    f.puts "#{gene[:symbol]}\t#{gene[:chr]}\t#{gene[:start]}\t#{gene[:end]}\t#{gene[:strand]}\t#{gene[:pausing_index]}\t#{gene[:pausing_area_rpkm]}\t#{gene[:ds_area_rpkm]}"
   end
 end
