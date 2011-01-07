a8ef0ca481a7683f86a9060268b9860d81f1b093
Ron Gejman <rsg2119@columbia.edu>
2011-01-07 18:07:56 -0500

    grep reports lines in base-1

--- analysis_scripts/wigreaderfast.rb
+++ analysis_scripts/wigreaderfast.rb
@@ -26,16 +26,14 @@ class WigReaderFast < WigReader
       next_header_pos = header_pos_index < (headers.length-1) ? header_pos_index+1 : nil
       rd, wr = IO.pipe
       fork do
-        puts lines.length
         if next_header_pos.nil?
           lines = lines[header_pos..-1]
         else
           lines = lines[header_pos...next_header_pos] #Trim the array; keep only the lines for my header
         end
         line = lines.shift
-        puts line
         raise "ERROR: This was supposed to be a header line. Instead got: #{line} for #{header_pos}." if line == nil or line[0,1] != "v"
-        puts "Got header (#{line}) for pos #{header_pos}"
+        puts "Got header (#{line}) for pos #{header_pos}. next_header_pos: #{next_header_pos}"
         tmp, chr, step = line.split(" ").collect{|a| a.split("=")[1]}
         step = step.to_i
         d = {}
