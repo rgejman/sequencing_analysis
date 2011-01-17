1498eccedd73f4fd554d39bf7c239484e935cd7a
Ron Gejman <rsg2119@columbia.edu>
2011-01-17 01:14:10 -0500

    readline != gets

--- analysis_scripts/shorten_alignment.rb
+++ analysis_scripts/shorten_alignment.rb
@@ -41,7 +41,7 @@ for i in (0...num_alignments)
     output_file_tokens = alignment_file.split(".")
     output_file = output_file_tokens.shift + ".approx_#{min_length}." + output_file_tokens.join(".")
     Open3.popen3("bamtools filter -in #{alignment_file} -isMapped true | samtools view -h -") do |i_stdin, i_stdout, i_stderr|
-      Open3.popen3("samtools view -hbS -o #{output_file} -") do |o_stdin, o_stdout, o_stderr|
+      Open3.popen3("samtools view -hbS - | samtools sort - #{output_file}") do |o_stdin, o_stdout, o_stderr|
         while (line = i_stdout.gets)
           if line[0,1] != "@"
             next if rand() > p
