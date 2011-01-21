2cdc46f7d8272f8ca8c7c15e157fe64b6926602d
Ron Gejman <rsg2119@columbia.edu>
2011-01-21 14:56:38 -0500

    added some functions for R plotting

--- analysis_scripts/analysis_functions.r
+++ analysis_scripts/analysis_functions.r
@@ -131,7 +131,7 @@ make_heatmaps = function(data, breaks, cols_per_heatmap, clusters, postfix, colo
 				colors = rev(gray(0:(length(brk)-2)/(length(brk)-2)))
 			}
 			else if(color_function == "redgreen") {
-				colors = redgreen((length(brk)-2)/(length(brk)-2))
+				colors = redgreen(length(brk)-1)
 			}
 			data_plot = t(d[,as.vector(cols_per_heatmap[,n])]) # transpose the array
 			nr = nrow(data_plot)
