8fd167db1986d61f5440fa28d2f88136e416290c
Ron Gejman <rsg2119@columbia.edu>
2011-01-21 18:17:43 -0500

    added some functions for R plotting

--- analysis_scripts/analysis_functions.r
+++ analysis_scripts/analysis_functions.r
@@ -60,7 +60,7 @@ read_table_remove_cols_set_row_names = function(file,cols,sep="	",quote="",heade
 }
 
 merge_all = function(data_frames, all.x=FALSE,all.y=FALSE,sort=FALSE,by="row.names"){
-	data = merge(data_frames[1], data_frames[2], all.x=all.x,all.y=all.y,sort=sort, by=by)
+	data = merge(data_frames[[1]], data_frames[[2]], all.x=all.x,all.y=all.y,sort=sort, by=by)
 	data_frames = data_frames[-(1:2)]
 	for(item in data_frames) {
 		data = merge(data,item, all.x=all.x,all.y=all.y,sort=sort, by=by)
