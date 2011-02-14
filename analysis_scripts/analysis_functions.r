5916dd539346d16ced7b3a1a0e03a04ba1781878
Ron Gejman <rsg2119@columbia.edu>
2011-02-13 22:33:13 -0500

    Reduced bowtie threads to 22

--- analysis_scripts/analysis_functions.r
+++ analysis_scripts/analysis_functions.r
@@ -16,8 +16,8 @@ smoothscatter_cor = function(x,y,mark,xlab,ylab,x_low_lim,x_high_lim,y_low_lim=N
 	main = paste(sep=" ", mark, xlab, "v", ylab)
 	png(paste(sep=".",main,"png"), width=1000, height=1000)
 	c = cor(x, y, method = "pearson")
-	col =  colorRampPalette(RColorBrewer::brewer.pal(9,"YlOrRd" ) ),bandwidth = bw )
-	smoothScatter(x, y, bandwidth=bw, main=main,  xlab=xlab, ylab=ylab, ylim=c(y_low_lim,y_high_lim), xlim=c(x_low_lim,x_high_lim))
+	col =  colorRampPalette(RColorBrewer::brewer.pal(9,"YlOrRd" ))
+	smoothScatter(x, y, bandwidth=bw, main=main,  xlab=xlab, ylab=ylab, ylim=c(y_low_lim,y_high_lim), xlim=c(x_low_lim,x_high_lim),colram=col)
 	if(line) {
 		abline(0,1, col="black") # regression line (y~x) 
 	}
