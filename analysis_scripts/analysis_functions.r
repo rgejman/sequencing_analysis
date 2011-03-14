416e0ce1768b296c7c049354e4e7c333eae978c1
Ron Gejman <rsg2119@columbia.edu>
2011-03-14 13:13:02 -0400

    Added a redblue color option

--- analysis_scripts/analysis_functions.r
+++ analysis_scripts/analysis_functions.r
@@ -232,6 +232,13 @@ make_heatmaps = function(data, breaks, cols_per_heatmap, clusters, postfix, colo
 				}
 				colors = maPalette(low="green", high="red",mid="white",k=(length(brk)-1))
 			}
+			else if(color_function == "redblue") {
+				if(even(length(brk))) {
+					step = brk[length(brk)] - brk[length(brk)-1]
+					brk = c(brk,brk[length(brk)]+step)
+				}
+				colors = maPalette(low="blue", high="red",mid="white",k=(length(brk)-1))
+			}
 			data_plot = t(d[,as.vector(cols_per_heatmap[,n])]) # transpose the array
 			nr = nrow(data_plot)
 			nc = ncol(data_plot)
